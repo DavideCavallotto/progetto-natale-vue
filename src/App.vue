0a49f79bb54664d3109c2e8037527390988bcfef
DavideCavallotto <davidecavallotto92@gmail.com>
2024-01-08 18:03:05 +0100

    ADD project team 2 Pokemon

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import MainPokemon from './components/MainPokemon.vue'
+
+</script>
+
+<template>
+  <MainPokemon/>
+
+  
+</template>
+
+<style lang="scss">
+@import "/node_modules/bootstrap/scss/bootstrap";
+</style>
---
+++ src/components/MainPokemon.vue
@@ -0,0 +1,50 @@
+
+<script setup>
+import axios from 'axios';
+import { ref, onMounted } from 'vue'
+
+
+
+let pokedex = ref(null)
+
+
+
+onMounted(() => {
+    console.log(axios)
+
+    axios.get('https://pokeapi.co/api/v2/pokemon')
+        .then(function (res) {
+
+            pokedex.value = res.data.results
+            console.log(res)
+        })
+})
+
+
+
+</script>
+
+<template>
+    <div>
+        <h1>Pokedex</h1>
+        <div class="container">
+            <div class="row">
+                <div class="col-3" v-for="pokemon in pokedex">
+                    <div class="card" style="width: 18rem;">
+                        <img :src="pokemon.url" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">{{pokemon.name}}</h5>
+                            
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+        
+    </div>
+</template>
+
+<style></style>
